200c9cade3f6e4d4a716324155513d4582dc7fe7
Mortal0110 <76845019+Mortal0110@users.noreply.github.com>
2023-04-28 11:27:00 +0800

    Update temp.js

--- Script/temp.js
+++ Script/temp.js
@@ -1,29 +1,70 @@
 
-// ==UserScript==
-// @name         哔哩哔哩首页推荐内容限制脚本
-// @namespace    bili-homepage-limit
-// @version      1
-// @description  限制哔哩哔哩首页推荐内容数量在前10条以内，并只保留指定类型的内容，如视频类型。
-// @author       OpenAI Sage
-// @match        https://app.bilibili.com/x/v2/feed/index
-// @grant        none
-// ==/UserScript==
+const url = $request.url;
+const notifyTitle = "bilibili-json";
 
-if ($response.body) {
-  let bodyJson = JSON.parse($response.body);
-  let feed = bodyJson.data || {};
-  let items = feed.items || [];
-
-  // 设置需要显示的视频数目
-  const MAX_VIDEO_COUNT = 2;
-  if (items.length > MAX_VIDEO_COUNT) {
-    // 仅保留前10个视频
-    items = items.slice(0, MAX_VIDEO_COUNT);
-    feed.items = items;
-
-    // 更新响应数据
-    $done({ body: JSON.stringify(bodyJson) });
-  }
-} else {
-  $done({});
+if (!$response.body) {
+    console.log(`$response.body为undefined:${url}`);
+    $done({});
 }
+
+let body = JSON.parse($response.body);
+
+if (!body.data) {
+    console.log(`body:${$response.body}`);
+    $notification.post(notifyTitle, url, "data字段错误");
+} else {
+    if (url.includes("x/v2/splash")) {
+        console.log('开屏页' + (url.includes("splash/show") ? 'show' : 'list'));
+        if (!body.data.show) {
+            console.log('数据无show字段');
+        } else {
+            delete body.data.show;
+            console.log('成功');
+        }
+    } else if (url.includes("x/v2/feed/index")) {
+        console.log('推荐页');
+        // 设置需要显示的视频数目
+        const MAX_VIDEO_COUNT = 10;
+        if (!body.data.items?.length) {
+            console.log(`body:${$response.body}`);
+            $notification.post(notifyTitle, '推荐页', "items字段错误");
+        } else {
+            body.data.items = body.data.items.filter(i => {
+                const {card_type: cardType, card_goto: cardGoto} = i;
+                if (cardType && cardGoto) {
+                    if (cardType === 'banner_v8' && cardGoto === 'banner') {
+                        if (!i.banner_item) {
+                            console.log(`body:${$response.body}`);
+                            $notification.post(notifyTitle, '推荐页', "banner_item错误");
+                        } else {
+                            for (const v of i.banner_item) {
+                                if (!v.type) {
+                                    console.log(`body:${$response.body}`);
+                                    $notification.post(notifyTitle, '推荐页', "type错误");
+                                } else {
+                                    if (v.type === 'ad') {
+                                        console.log('banner广告');
+                                        return false;
+                                    }
+                                }
+                            }
+                        }
+                    } else if (cardType === 'small_cover_v5' && cardGoto === 'av') {
+                        if (i.goto_impress?.length) {
+                            i.goto_impress = i.goto_impress.filter(g => !g?.includes('ad'));
+                        }
+                        if (i.goto?.length) {
+                            i.goto = i.goto.filter(g => !g?.includes('ad'));
+                        }
+                        return true;
+                    }
+                }
+                return false;
+            });
+            // 仅保留前10个视频
+            body.data.items = body.data.items.slice(0, MAX_VIDEO_COUNT);
+        }
+    }
+}
+
+$done({ body: JSON.stringify(body) });
