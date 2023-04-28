98b6e488b7ba597a466677179653583ecaa0953a
Mortal0110 <76845019+Mortal0110@users.noreply.github.com>
2023-04-28 11:02:01 +0800

    Create temp.js

---
+++ Script/temp.js
@@ -0,0 +1,29 @@
+
+// ==UserScript==
+// @name         哔哩哔哩首页推荐内容限制脚本
+// @namespace    bili-homepage-limit
+// @version      1
+// @description  限制哔哩哔哩首页推荐内容数量在前10条以内，并只保留指定类型的内容，如视频类型。
+// @author       OpenAI Sage
+// @match        https://app.bilibili.com/x/v2/feed/index
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const MAX_LIMIT = 10; // 最大限制条数
+    const ALLOWED_TYPE = [2]; // 允许的内容类型，如视频类型为2
+
+    const data = $response.bodyResult.data;
+    const filteredData = data.filter(item => ALLOWED_TYPE.includes(item.type)).slice(0, MAX_LIMIT);
+
+    $done({
+        body: JSON.stringify({
+            code: 0,
+            message: "OK",
+            ttl: 1,
+            data: filteredData
+        })
+    });
+})();
