644e6e73e095d5959c71e34a9ac55d5256d63fd2
Mortal0110 <76845019+Mortal0110@users.noreply.github.com>
2023-07-10 20:24:09 +0800

    Create Block100SVIPCrack.js

---
+++ Script/Block100SVIPCrack.js
@@ -0,0 +1,6 @@
+
+var body = $response.body;
+body = "lvCQG8cCxqficLk+LttK+L2YRSLM39a3jj+Mfoba4wNaV8x54RYUAWrJvcwwFZemFEWG5xK2FlI8k2hlHsSW7uaAeW45GPxSPt1uvVwPI5zLiMJuHvGycPsqS+ozuIELZnJYBJk2unXdxQCbEof4Rmgo3m7O+4ByRFtdbPMM/COhtxmSVeHXnNcSfVlkALSnTyKl0xORwbczE2UdkNg/uioD0AtxIrGJ4jaUVNVKvJsibH0dOKL/bljPSrG31RQt";
+$done({
+body
+});
